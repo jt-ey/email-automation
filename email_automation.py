ceb5eaa06da3e32e8950372007c1cbaa58059e15
jt-ey <89822398+jt-ey@users.noreply.github.com>
2021-08-31 15:29:41 +1000

    Add files via upload

--- email_automation.py
+++ email_automation.py
@@ -9,35 +9,56 @@ workbook_name = '\sample.xlsx' # Name of your excel file, in current working dir
 with open('email_text.json') as f: # Load email content data as dictionary
     text_dict = json.load(f)
     
+CCs = ['janelle.tang@au.ey.com', # CCed Recipients
+      'janelle.tang@au.ey.com'] 
 
-def create_mail(text, recipient_name, data, subject, recipient_email, attachment = False,send=False,):
+
+def convert_list_to_recipients(lst):
+    result = ""
+    for email in lst:
+        result = result + email + ';'
+    return result
+
+def create_mail(text, recipient_name, data, subject, recipient_email, location = None, app = None, attachment = False,send=False,):
     """
     @params:
+    
     """
     outlook = win32.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
     mail.To = recipient_email
-    mail.CC = 'email@email.com'
+    mail.CC = convert_list_to_recipients(CCs)
     mail.Subject = subject
+    
+    # fill in name
     recipient_name = recipient_name.replace('-',' ').title() 
-    text = text.replace('{name}',recipient_name) # Add name to Email body
+    text = text.replace('{name}',recipient_name) 
+    
+    # fill in date
+    future_date = (dt.datetime.now().date()+dt.timedelta(days=3)).strftime("%d-%b-%Y")
+    text = text.replace('{future date}', future_date)
+    
+#     # fill in location
+#     if app:
+#         text = text.replace('{application}')
+    
+#     # fill in app
+#     if location:
+#         text = text.replace('{location}')
     
     if attachment:
         attachment_path = PATH+r'\attachments\{}'.format(data)
         mail.HtmlBody = text.replace('{name}',recipient_name)
         mail.Attachments.Add(attachment_path)
     else:
-        data = data.style.set_properties(**{'background-color': '#ffffbf'}, subset=['Service Account Still Required? (Y/N)',
-                                             'Updated Owner Name',
-                                             'Remarks (if any)']).hide_index().render(index=False)
         
         ## Remove Null values
         data = data.replace('nan','')
         
         ## Styling
         data = data.replace('<table','<table border = 1 class="dataframe"')
-        
         mail.HtmlBody = text.replace('{table}',data)
+
     if send:
         mail.send()
     else:
@@ -53,7 +74,6 @@ def get_email_content(attachment,email_type):
     return body, subject
 
 
-
 df = pd.read_excel(PATH+workbook_name)
 email_list = df.Email.unique().tolist() # All people that need to be email (no duplicates)
 
@@ -64,16 +84,26 @@ for email in email_list:
     
     email_type_list = data.Type.unique()
     for email_type in email_type_list: # Split the types of emails to send (types of Orphan Accounts)
-        table = data[data.Type == email_type][['Domain',
-                                             'SamAccountName',
-                                             'DisplayName',
-                                             'Description',
-                                             'Previous Owner employee ID',
-                                             'Previous Owner Name',
-                                             'Service Account Still Required? (Y/N)',
-                                             'Updated Owner Name',
-                                             'Remarks (if any)']]
+        
+        ## Highlighted columns for recipient to fill - in the email_text.JSON
+        required_cols = text_dict[email_type]['attachment']['columns']
+        required_cols = required_cols.split(", ")
+        
+        select_cols = ['Domain',
+                     'SamAccountName',
+                     'DisplayName',
+                     'Description',
+                     'Previous Owner employee ID',
+                     'Previous Owner Name'] + required_cols
+        
+        
+        table = data[data.Type == email_type][select_cols]
         n_rows = table.shape[0]
+        
+        # Styling 
+        table.reset_index(drop=True)
+        table = table.style.set_properties(**{'background-color': '#ffffbf','border':' 1pt solid #808080'}, subset=required_cols)
+        
         if n_rows > 5:
             """
             If more than 5 rows, create an attachment file
@@ -83,12 +113,14 @@ for email in email_list:
             
             ## name of attachment
             attachment_name = "{}_{}.xlsx".format(email_type,name)
-            table.to_excel('attachments/{}'.format(attachment_name))
+            table.to_excel('attachments/{}'.format(attachment_name),index=False)
+            
             create_mail(body,name, attachment_name, subject, email, attachment = True)
             
-        elif n_rows > 0: # Just precautionary measure incase there are empty DFs
+        elif n_rows > 0:
             """
             Otherwise, just keep it as a table
             """
+            html = table.render(index=False)
             body, subject = get_email_content(False,email_type)
-            create_mail(body, name,table, subject, email)
+            create_mail(body, name,html, subject, email)
